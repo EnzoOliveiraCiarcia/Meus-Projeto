f7080df4a86c7c31718a9b29dc664515a574db18
Enzo Oliveira Ciarcia <enzooliveiraciarcia@gmail.com>
2026-03-30 20:08:44 -0300

    Add files via upload

--- Exercicio2.java
+++ Exercicio2.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
-
 public class Exercicio2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main (String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int numero1;
+    int numero2;
+    System.out.println ("Digite o primeiro número");
+    numero1 = scanner.nextInt();
+    System.out.println ("Digite o segundo número");
+    numero2 = scanner.nextInt();
 
-        int numero;
-
-        System.out.println("Digite um número inteiro: ");
-        numero = scanner.nextInt();
-
-        int triplo, dobro;
-        triplo = numero * 3;
-        dobro = numero * 2;
-
-        int resultado;
-        resultado = (triplo + 1) + (dobro - 1);
-
-        System.out.println("Resultado: " + resultado);
-
+ if (numero1 > numero2) {
+    System.out.println("O primeiro numero é maior");
+    } else if (numero1 == numero2) {
+    System.out.println("Os numeros sao iguais");
+   } else {
+    System.out.println("O segundo numero é maior");
+}
         scanner.close();
     }
 }
+
