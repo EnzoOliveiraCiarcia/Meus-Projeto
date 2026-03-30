9a4e3e43e4b9e4d943dd8d9608d3456bc102a0dd
Enzo Oliveira Ciarcia <enzooliveiraciarcia@gmail.com>
2026-03-30 20:08:24 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Exercicio1 {
+    public static void main (String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int numero1;
+    int numero2;
+    System.out.println ("Digite o primeiro número");
+    numero1 = scanner.nextInt();
+    System.out.println ("Digite o segundo número");
+    numero2 = scanner.nextInt();
+
+        if (numero1 == numero2) {
+            System.out.println("Os numeros sao iguais");
+        } else {
+            System.out.println("Os numeros sao diferentes");
+        }
+
+        scanner.close();
+    }
+}
+
