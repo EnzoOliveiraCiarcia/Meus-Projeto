ab1f4cd30bfc569b80c9a4c5a7f5a8db6af79e44
Enzo Oliveira Ciarcia <enzooliveiraciarcia@gmail.com>
2026-03-30 20:09:22 -0300

    Rename Exercicio2 to MaiorOuIgual and refactor code

--- Exercicio2.java
+++ Exercicio2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Exercicio2 {
+public class MaiorOuIgual {
     public static void main (String[] args){
     Scanner scanner = new Scanner(System.in);
     int numero1;
