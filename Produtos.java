86066601d9c9a41411350cf55e28ab7ee7947e5f
Enzo Oliveira Ciarcia <enzooliveiraciarcia@gmail.com>
2026-04-07 19:19:49 -0300

    Add files via upload

---
+++ Produtos.java
@@ -0,0 +1,13 @@
+//ler um nome de um produto e exibir seu preço
+
+import java.util.Scanner;
+ 
+public class Produtos {
+    publicstatic void main (String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha o seu produto:\nnotebook\nmonitor\nmouse");
+        String produto = scanner.nextline();
+        double preço;
+        switch 
+    }
+ }
