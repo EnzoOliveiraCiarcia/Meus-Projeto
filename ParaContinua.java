fdf253b02be68489567a79a9615908e204f30e02
Enzo Oliveira Ciarcia <enzooliveiraciarcia@gmail.com>
2026-04-13 20:06:11 -0300

    Update ParaContinua.java

--- ParaContinua.java
+++ ParaContinua.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
 public class ParaContinua {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {       
         Scanner scanner = new Scanner(System.in);
-        String ordem;
-        
-        do {
-            System.out.print("digite PARE se quiser parar: ");
+        String ordem;  
+        // do {
+        //     System.out.print("digite PARE se quiser parar: ");
+        //     ordem = scanner.nextLine();
+        // } while (!(ordem.toUpperCase().equals("PARE")));
+
+System.out.print ("digite PARE se quiser parar: ");
+ordem = scanner.nextLine();
+        while (!(ordem.toUpperCase().equals("PARE"))) {
+            System.out.print ("digite PARE se quiser parar: ");
             ordem = scanner.nextLine();
-        } while (!(ordem.toUpperCase().equals("PARE")));
-        
+        }
     }
 }
